5cb7f97c1276172be5032acf29f165d8b7178e8a
simthj591-blip <simthj591@gmail.com>
2026-09-16 18:01:22 +0800

    Add files via upload
    上传手机读表器

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='ammeter-reader-v1',F=['./','index.html','app.js','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
