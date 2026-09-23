0927cb6ec5946c03fd29d1469cf68e0c62412ba9
simthj591-blip <simthj591@gmail.com>
2026-09-23 18:51:49 +0800

    Refresh cached reader application

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='ammeter-reader-v1',F=['./','index.html','app.js','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='ammeter-reader-v2',F=['./','index.html','app.js','manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
